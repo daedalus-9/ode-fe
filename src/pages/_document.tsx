bb4556bc072030dc8074fd590d42077c0d2db55f
Adam <adamyoungy678@gmail.com>
2026-04-06 13:45:09 +0100

    fix: update favicon link to use new logo and remove old favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/odx-logo.png" />
 
         <Script id="gtm-script" strategy="beforeInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
